33af8e97fda6ea79bb7394eb722ace89a6e0f9a0
Adarsh tripathi <adarshtripathi2070@gmail.com>
2024-09-26 21:41:49 +0530

    commit

--- src/app/Contactus/page.jsx
+++ src/app/Contactus/page.jsx
@@ -7,16 +7,20 @@ const page = () => {
   // list of cities
   const cities = ["mumbai", "algeria", "kolkata", "Delhi", " Chennnai", "Benaluru", "Lucknow", "pune", "Kanpur", "Jaipur", "Indore", "Hyderabad"]
 
-
   // useState for the state management
   const [searchcity, setSearchcity] = useState('');
+
+  // input field change
+  const handleOnChange = (e) => {
+    setSearchcity(e.target.value)
+  }
 
   // filterCities according to search input
   const filterCities = cities.filter(city => city.toLowerCase().includes(searchcity.toLowerCase()))
 
-
-  const handleOnChange = (e) => {
-    setSearchcity(e.target.value)
+  // button on click 
+  const handleClickEvent = () => {
+    filterCities()
   }
   return (
     <>
@@ -26,17 +30,17 @@ const page = () => {
         </section>
         <form action="" className='w-[53%] text-end space-x-5 space-y-5'>
           <input type="search" value={searchcity} name="searchCity" id="city" className='p-3 text-blue-700 rounded-lg ' onChange={handleOnChange} />
-          <button className="search p-2 bg-blue-900 border-2 border-blue-500 rounded-lg inset-3 ">
+          <button className="search p-2 bg-blue-900 border-2 border-blue-500 rounded-lg inset-3 " onClick={handleClickEvent}>
             SEARCH
           </button>
         </form>
         <ul className="w-[57%] text-center">
           {/* map method fo the list */}
           {
-            filterCities.map((val) => {
+            filterCities.map((val, index) => {
               return (
                 <>
-                  <li className='mb-2'>{val}</li>
+                  <li key={index} className='mb-2'>{val}</li>
                 </>
               )
             })
